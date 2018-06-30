e32012e8c82c7cb26da588792a3c889c1377004f
huanwangkeji <wangyuxi@huan.tv>
2018-06-30 16:18:24 +0800

    项目初始化导入

---
+++ src/main/java/tv/huan/cms/CmsFrameworkApplication.java
@@ -0,0 +1,12 @@
+package tv.huan.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsFrameworkApplication.class, args);
+	}
+}
